e3f4b38e781a760779534b263107e9c34db20c46
RyanSmith19 <lifewithryans@gmail.com>
2023-03-01 16:50:31 -0800

    init

---
+++ src/test/MainTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Assertions;
+
+public MainTest {
+    @Test
+    void hypothenuse(){
+        Assertions.assertEquals(Main.hypothenuse(10,20),22.360679774997898);
+    }
+}
